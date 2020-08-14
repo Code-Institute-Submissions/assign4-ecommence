ed2bbccc06ffb796c52cc5ee89319b5b3f4b98f1
skyeoh06 <felyciayeoh@yahoo.com>
2020-08-14 13:03:13 +0000

    create update and delete route fork books and authors

--- BooksTreasureProject/urls.py
+++ BooksTreasureProject/urls.py
@@ -24,9 +24,13 @@ urlpatterns = [
     path('books/', books.views.index, name="index"),
     path('books/all', books.views.all_books, name="books"),
     path('books/create', books.views.create_book, name="create_book"),
-    path('books/update/<book_id>',books.views.update_book, name="update_book"),
+    path('books/update/<book_id>', books.views.update_book, name="update_book"),
+    path('books/delete/<book_id>', books.view.delete_book, name="delete_book"),
     path('authors/', books.views.all_authors, name="all_authors"),
     path('authors/create', books.views.create_author, name="create_author"),
-    path('authors/update/<author_id>',books.views.update_author, name="update_author"),
+    path('authors/update/<author_id>',
+         books.views.update_author, name="update_author"),
+    path('authors/delete/<author_id>',
+         books.views.delete_author, name="delete_author"),
     path('carts/', carts.views.cart, name="carts")
 ]
--- books/views.py
+++ books/views.py
@@ -37,20 +37,37 @@ def all_books(request):
     })
 
 # update books page
+
+
 def update_book(request, book_id):
-    book=get_object_or_404(Book,pk=book_id)
-    #Submitted form
-    if request.method=="POST":
-        form=BookForm(request.POST, instance=book)
+    book = get_object_or_404(Book, pk=book_id)
+    # Submitted form
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
         form.save()
         return redirect(reverse(all_books))
     else:
-        #extract the data from database
-        form=BookForm(instance=book)
-        return render(request, 'books/upate_book.template.html',{
-            'form':form
+        # extract the data from database
+        form = BookForm(instance=book)
+        return render(request, 'books/upate_book.template.html', {
+            'form': form
         })
+
+# delete books page
+
+
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    # submission
+    if request.method == "POST":
+        book.delete()
+        return redirect(reverse(all_books))
+    else:
+        return render(request, 'books/delete_book.template.html', {
+            'book': book})
 # create authors page
+
+
 def create_author(request):
     if request.method == "POST":
         # Submission data from User
@@ -74,18 +91,32 @@ def all_authors(request):
         'authors': all_authors
     })
 
-#update authors page
-def update_author(request,author_id):
-    author=get_object_or_404(Author,pk=author_id)
-    #submitted form
-    if request.method=="POST":
-        form=AuthorForm(request.POST,instance=author)
+# update authors page
+
+
+def update_author(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    # submitted form
+    if request.method == "POST":
+        form = AuthorForm(request.POST, instance=author)
         form.save()
         return redirect(reverse(all_authors))
     else:
-    #extract data from database
-        form=AuthorForm(instance=author)
-        return render(request,'books/update_author.template.html',{
-                      'form':form,
-                      'author':author
-        })    
+        # extract data from database
+        form = AuthorForm(instance=author)
+        return render(request, 'books/update_author.template.html', {
+                      'form': form,
+                      'author': author
+                      })
+# delete author page
+
+
+def delete_author(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    # submission
+    if request.method == "POST":
+        author.delete()
+        return redirect(reverse(all_authors))
+    else:
+        return render(request, 'books/delete_author.template.html', {
+            'author': author})

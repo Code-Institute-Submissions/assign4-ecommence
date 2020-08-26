88675b8662012e40063b3cb3bb0c07626a445255
skyeoh06 <felyciayeoh@yahoo.com>
2020-08-26 14:43:06 +0000

    Updated settings.py

--- BooksTreasureProject/settings.py
+++ BooksTreasureProject/settings.py
@@ -95,12 +95,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {'default': dj_database_url.parse(os.environ["DATABASE_URL"])}
 
 
 # Password validation
